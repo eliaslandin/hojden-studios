b45807d8779600a085dfbdac36575a7ab538576f
Elias Landin <elias.landin@me.com>
2024-05-01 14:24:37 +0200

    Installed shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,10 @@
 import Image from "next/image";
+import { Header } from "./components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <header>
-        <div className="flex flex-col items-center column gap-6">
-          <div>
-            <Image
-              alt="Höjden studios logga"
-              src="/hojden_webblogga.webp"
-              height={200}
-              width={700}
-            />
-          </div>
-          <div>
-            <nav>
-              <ul className="flex flex-row">
-                <li>
-                  <a href="/" className="border-r border-hojden-green px-8 py-4">Aktuellt</a>
-                </li>
-                <li>
-                  <a href="/" className="">Kalender</a>
-                </li>
-                <li>
-                  <a href="/" className="">Höjden sessions</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header/>
       
     </main>
   );
