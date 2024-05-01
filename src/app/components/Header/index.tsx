516c3bf8b48e0e0a27c47e8fd8984db44ac6943d
Elias Landin <elias.landin@me.com>
2024-05-01 14:14:56 +0200

    First commit

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export const Header = ({
+  className,
+}: {
+  className?: string;
+}) => {
+  return (
+    
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "hojden-green": "#006613"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
