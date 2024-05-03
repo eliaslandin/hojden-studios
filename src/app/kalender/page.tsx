0dc5d3c67ba92b651f5c44a1b2844a3c4285f878
Elias Landin <elias.landin@me.com>
2024-05-03 20:04:55 +0200

    Merge fix

--- src/app/kalender/page.tsx
+++ src/app/kalender/page.tsx
@@ -6,42 +6,6 @@ import { ParagraphLink } from "@/components/ParagraphLink";
 
 export default function Page() {
   return (
-<<<<<<< HEAD
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="container flex flex-col items-center">
-        <Post>
-        <Heading1 className="pt-10 text-hojden-orange">Händer på höjden</Heading1>
-          <PostBody>
-            <Heading3>17 February - members party! 18:00-late</Heading3>
-            <br/>
-            <Heading3>1 March - höjden nights (afterwork) 17.00</Heading3>
-            <br/>
-            <Heading3>25-27 March - höjden sessions (workshop)</Heading3>
-            Read more here: <ParagraphLink href="/daglig-traening">https://en.hojden.house/daglig-traening</ParagraphLink>
-            <br/>
-            <br/>
-            <Heading3>5 April - höjden nights (afterwork) 17.00</Heading3>
-            <br/>
-            <Heading3>9-11 April - höjden sessions (workshop)</Heading3>
-            Read more here: <ParagraphLink href="/daglig-traening">https://en.hojden.house/daglig-traening</ParagraphLink>
-            <br/>
-            <br/>
-            <Heading3>16-18 April - höjden sessions (workshop)</Heading3>
-            Read more here: <ParagraphLink href="/daglig-traening">https://en.hojden.house/daglig-traening</ParagraphLink>
-            <br/>
-            <br/>
-            <Heading3>27 April - höjden festival</Heading3>
-            Festival by höjdens members for all!
-            <br/>
-            <br/>
-            <Heading3>3 May - höjden nights (afterwork) 17.00</Heading3>
-          </PostBody>
-        </Post>
-        
-        
-      </div>
-    </main>
-=======
     <PageContent className="sm:items-center">
       <Heading1 className="text-hojden-orange">Händer på höjden</Heading1>
       <Heading3>17 February - members party! 18:00-late</Heading3>
@@ -64,6 +28,5 @@ export default function Page() {
       <br/>
       <Heading3>3 May - höjden nights (afterwork) 17.00</Heading3>
     </PageContent>
->>>>>>> dev
   );
 }
