1ecd5329b685e146fc78c6ef68c732e6040bf9ab
Elias Landin <elias.landin@me.com>
2024-05-09 08:42:10 +0200

    Added not found page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,27 @@
+import { SupportedLocale } from "@/types";
+import { Heading1 } from "@/components/Heading1";
+import { PageContent } from "@/components/PageContent";
+import { useLocale } from "next-intl";
+
+const content = {
+  en: () => (
+    <>
+      <Heading1>Page not found</Heading1>
+    </>
+  ),
+  sv: () => (
+    <>
+      <Heading1>Sidan hittades inte</Heading1>
+    </>
+  )
+}
+
+export default function NotFoundPage() {
+  const locale = useLocale()
+
+  return (
+    <PageContent>
+      { content[locale as SupportedLocale]() }
+    </PageContent>
+  )
+}
