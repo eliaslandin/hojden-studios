d98ffec6b0ce8e769e56bd8f638f01573795ae25
Elias Landin <elias.landin@me.com>
2024-05-09 16:17:37 +0200

    Added newsletter signup form

---
+++ src/components/NewsletterSignUpForm/index.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+export const NewsletterSignUpForm = ({
+  className
+}: {
+  className?: string;
+}) => {
+  return (
+    <iframe className="min-h-72 pt-4" title="Embedded Content" name="htmlComp-iframe" width="100%" height="100%" allow="fullscreen" data-src="" src="https://en-hojden-house.filesusr.com/html/23b603_08f87b247f0528278f51564a229aa76d.html"></iframe>
+  );
+};
