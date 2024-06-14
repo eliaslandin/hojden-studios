f7c2ea21eb740a1cd30b34e660622dc70fa67953
Elias Landin <elias.landin@me.com>
2024-06-14 08:11:52 +0200

    Fixed bugg where some Swedish characters weren't showing up correctly.

--- src/app/[locale]/om-hojden/page.tsx
+++ src/app/[locale]/om-hojden/page.tsx
@@ -10,7 +10,7 @@ const content = {
     <>
     <Heading1 className="text-hojden-orange">Our activity</Heading1>
     <p>
-    höjden is an interdisciplinary artist-driven platform for production, and daily work. A place for connection and context for performance, choreography, music and all kinds of artistic disciplines. We have performances, all kinds of events, and from spring 2024 a new public facing gallery space. We rent our premises at Östbergatorget 21 in Stockholm.<br/>
+    höjden is an interdisciplinary artist-driven platform for production, and daily work. A place for connection and context for performance, choreography, music and all kinds of artistic disciplines. We have performances, all kinds of events, and from spring 2024 a new public facing gallery space. We rent our premises at östbergatorget 21 in Stockholm.<br/>
     <br/>
     An innovative gathering place for freelance artists who pave the way for collective, norm-critical and experimental collaborations between fields and across borders. In addition to being a workplace, we also offer workshops, performances and lectures regularly. höjden aims to appeal to a wide audience and to supplement existing initiatives that are available to artists on their own terms. <br/>
     <br/>
@@ -25,9 +25,9 @@ const content = {
     <>
     <Heading1 className="text-hojden-orange">Vår verksamhet</Heading1>
     <p>
-    höjden är en interdisciplinär konstnärsdriven plattform för produktion, sammanhang och dagligt arbete främst inom dans, performance, och koreografi men även andra konstnärliga grenar såsom bildkonst och musik. Vi hyr våra lokaler på Östbergatorget 21 i Stockholm.<br/>
+    höjden är en interdisciplinär konstnärsdriven plattform för produktion, sammanhang och dagligt arbete främst inom dans, performance, och koreografi men även andra konstnärliga grenar såsom bildkonst och musik. Vi hyr våra lokaler på östbergatorget 21 i Stockholm.<br/>
     <br/>
-    höjden är en nyskapande samlingsplats för frilansande konstnärer som banar väg för kollektiva, normkritiska och experimentella samarbeten mellan fält och över gränser. Förutom att vara en arbetsplats erbjuder vi även workshops, föreställningar och föreläsningar regelbundet. Vi vill med vår publika verksamhet tilltala en bredare skara kulturintresserade och med angelägenhet komplettera det kulturutbud som finns att tillgå på konstnärers villkor.<br/>
+    höjden är en nyskapande samlingsplats för frilansande konstnärer som banar väg för kollektiva, normkritiska och experimentella samarbeten mellan fält och över gränser. Förutom att vara en arbetsplats erbjuder vi även workshops, föreställningar och föreläsningar regelbundet. Vi vill med vår publika verksamhet tilltala en bredare skara kulturintresserade och med angelägenhet komplettera det kulturutbud som finns att tillgå på konstnärers villkor.<br/>
     <br/>
     Vi har en intersektionell inställning till arbetsmiljö, lokal och organisation som genomsyrar alla aktiviteter, rekrytering av medlemmar och personal. Konstnärerna på höjden bestämmer tillsammans med styrelsen hur plattformens dagliga verksamhet ser ut och kan driva olika delar av sina projekt och sitt arbete genom höjden. höjden har i nuläget omkring 30 konstnärer som jobbar inom dans, musik, performance, koreografi, bildkonst och design.<br/>
     <br/>
